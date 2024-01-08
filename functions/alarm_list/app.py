19d07f6aad94688f96311c38e252fe283605638c
Zoran Pucar <zzorp@amazon.com>
2024-01-08 18:47:19 +0100

    Added controls for suppression of alarms.

--- functions/alarm_list/app.py
+++ functions/alarm_list/app.py
@@ -40,7 +40,7 @@ def filter_by_property(alarms, prop, value):
 
 def get_filter_icon(color_code):
     return f'''<?xml version="1.0" encoding="iso-8859-1"?>
-            <svg fill="#{color_code}" height="15px" width="15px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            <svg fill="#{color_code}" height="12px" width="12px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                  viewBox="0 0 300.906 300.906" xml:space="preserve">
             <g>
                 <g>
@@ -52,6 +52,23 @@ def get_filter_icon(color_code):
                 </g>
             </g>
             </svg>'''
+
+
+def get_suppress_icon():
+    return f'''<svg fill="#9a9898" height="12px" width="12px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 27.965 27.965" xml:space="preserve">
+    <g id="SVGRepo_bgCarrier" stroke-width="0">
+    </g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier"> <g>
+    <g id="c142_x">
+    <path d="M13.98,0C6.259,0,0,6.261,0,13.983c0,7.721,6.259,13.982,13.98,13.982c7.725,0,13.985-6.262,13.985-13.982
+    C27.965,6.261,21.705,0,13.98,0z
+    M19.992,17.769l-2.227,2.224c0,0-3.523-3.78-3.786-3.78c-0.259,0-3.783,3.78-3.783,3.78
+    l-2.228-2.224c0,0,3.784-3.472,3.784-3.781c0-0.314-3.784-3.787-3.784-3.787l2.228-2.229c0,0,3.553,3.782,3.783,3.782
+    c0.232,0,3.786-3.782,3.786-3.782l2.227,2.229c0,0-3.785,3.523-3.785,3.787C16.207,14.239,19.992,17.769,19.992,17.769z">
+    </path>
+    </g>
+    <g id="Capa_1_104_"> </g> </g> </g></svg>'''
 
 
 def lambda_handler(event, context):
@@ -232,7 +249,10 @@ def lambda_handler(event, context):
 
         aux_html += f'<hr /><h4>Metric Info</h4>'
 
-        html += f'<td>{alarm["detail"]["alarmName"]}<br />'
+        html += f'''<td><a>{get_suppress_icon()}</a><cwdb-action action="call" confirmation="WARNING! THIS WILL SUPPRESS THIS ALARM"
+                display="popup" endpoint="{configurator_lambda_function}">
+            {{ "suppress": "{alarm['alarmKey']}" }}
+            </cwdb-action> {alarm["detail"]["alarmName"]}<br />'''
         if "metrics" in alarm["detail"]["configuration"]:
             for metric in alarm["detail"]["configuration"]["metrics"]:
                 if 'expression' in metric:
